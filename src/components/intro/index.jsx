05b5ea8107f5cbd563f9d397ef72d4844d7d4574
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-04-06 20:10:25 +0530

    :lock: The Styling is started :recycle:

--- src/components/intro/index.jsx
+++ src/components/intro/index.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './style.scss';
+import cloud from '../../images/cloud.png';
+import cloudSoft from '../../images/cloud-soft.png';
 
 const Intro = () => {
   return (
     <div className="Intro-section">
-
+      <div 
+        className="vector-bg" 
+        id='parallax'
+      ></div>
+      <img 
+        src={cloud} 
+        alt="" 
+        className='cloud-soft' 
+      />
+      <img 
+          src={cloudSoft} 
+          alt="" 
+          className='cloud-soft' 
+      />
+      <div className="content"></div>
     </div>
   )
 }

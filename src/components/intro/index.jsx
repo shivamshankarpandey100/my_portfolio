7674001aec8ab43c2a279932f3b6f7459fcdff00
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-04-07 16:03:20 +0530

    :rotating_light: the left column is ready :rotating_light:

--- src/components/intro/index.jsx
+++ src/components/intro/index.jsx
@@ -3,6 +3,7 @@ import './style.scss';
 import cloud from '../../images/cloud.png';
 import cloudSoft from '../../images/cloud-soft.png';
 import Navigation from './navigation-bar';
+import IntroContent from './intro-content'
 const Intro = () => {
   return (
     <div className="Intro-section">
@@ -22,6 +23,7 @@ const Intro = () => {
       />
       <div className="content">
         <Navigation/>
+        <IntroContent/>
       </div>
     </div>
   )
---
+++ src/components/intro/intro-content/index.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import './style.scss';
+import { BsAwardFill } from 'react-icons/bs';
+import {FaUser} from 'react-icons/fa';
+import hand from "../../../images/hand.png";
+import CallToAction from '../../shared/CallToAction';
+import boy from '../../../images/boy.png';
+
+const IntroContent = () => {
+  return (
+    <div className="intro-content">
+        <div className="layout">
+          <div className="left-col">
+            <h1 className="title">
+                <span className="small-text">
+                    <span className="text">Hello</span>
+                    <span className="icon">
+                        <img src={hand} alt="" />
+                    </span>
+                    <span className="text">, I Am</span>
+                </span>
+                <span className="big-text">Shivam Shankar Pandey</span>
+            </h1>
+            <p>
+            I am a 💻 Computer 👨🏻‍💻 Science Student . My Interested in: 💻 Computer Science, 🧠 Competitive Programming & 🐍 Web Development and AI&ML
+            </p>
+            <CallToAction text="Contact Me"/>
+          </div>  
+          <div className="right-col">
+            <img src={boy} alt="userimage" />
+
+            <div className="highlights horizontal">
+                    <div className="icon">
+                        <BsAwardFill />
+                    </div>
+                    <div className="text">Best Design Award
+                    </div>
+            </div>
+            <div className="highlights verticle">
+                <div className="icon">
+                    <FaUser />
+                </div>
+                <div className="text">
+                    <span>4k+</span>
+                        Happy Coustomers
+                </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default IntroContent

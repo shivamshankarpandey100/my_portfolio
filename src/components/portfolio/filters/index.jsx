ee861f4170e56341647a754238479250eef941cb
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-04-11 14:27:46 +0530

    :card_file_box::card_file_box: active class added :card_file_box::card_file_box:

--- src/components/portfolio/filters/index.jsx
+++ src/components/portfolio/filters/index.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
+import "./style.scss";
 
 const filtersData = [
     {
         name: "All",
         id: "all",
+    },
+    {
+        name: "Products",
+        id: "product",
     },
     {
         name: "Web Pages",
@@ -21,11 +26,21 @@ const filtersData = [
 
 
 const Filters = () => {
+
+    const [active, setActive] = useState("");
+    const clickHandler = (id) => {
+        setActive(id);
+    }
   return (
-    <ul className="filters-menu-items">
-        {filtersData.map((item,index)=>{
+    <ul className="filter-menu-items">
+        {filtersData.map((item)=>{
             return (
-            <li className="filter-menue-item"></li>
+            <li key={item.id} 
+            className={`filter-menu-item ${active === item.id ? "active" : ""}`}
+            onClick={()=>clickHandler(item.id)}
+                >
+                {item.name}
+                </li>
         )
         })}
         
--- src/components/portfolio/index.jsx
+++ src/components/portfolio/index.jsx
@@ -1,10 +1,81 @@
 import React from 'react';
 import Section from '../shared/section';
+import Filters from './filters';
 import './style.scss';
+
+const projectsData = [
+    {
+        id: 1,
+        name: "Healthy Food Restraunt",
+        tags: ["web-app", "mobile-app"],
+        media: {
+            thumbnail: require("../../images/portfolio/thumb-7.jpg"),
+        },
+    },
+    {
+        id: 2,
+        name: "Anna & Daniel",
+        tags: ["web-page"],
+        media: {
+            thumbnail: require("../../images/portfolio/thumb-2.jpg"),
+        },
+    },
+    {
+        id: 3,
+        name: "Web Design Landing Page",
+        tags: ["web-page"],
+        media: {
+            thumbnail: require("../../images/portfolio/thumb-8.jpg"),
+        },
+    },
+    {
+        id: 4,
+        name: "Business Analytics Web App",
+        tags: ["web-app", "mobile-app"],
+        media: {
+            thumbnail: require("../../images/portfolio/thumb-1.jpg"),
+        },
+    },
+    {
+        id: 5,
+        name: "Limitless",
+        tags: ["web-app", "web-page"],
+        media: {
+            thumbnail: require("../../images/portfolio/thumb-6.jpg"),
+        },
+    },
+    {
+        id: 6,
+        name: "Dashboard",
+        tags: ["product", "web-app", "mobile-app"],
+        media: {
+            thumbnail: require("../../images/portfolio/thumb-4.jpg"),
+        },
+    },
+    {
+        id: 7,
+        name: "Digital Creative Agency",
+        tags: ["web-app"],
+        media: {
+            thumbnail: require("../../images/portfolio/thumb-3.jpg"),
+        },
+    },
+    {
+        id: 8,
+        name: "Virtual Reality Experience",
+        tags: ["web-app", "mobile-app", "web-page"],
+        media: {
+            thumbnail: require("../../images/portfolio/thumb-5.jpg"),
+        },
+    },
+];
+
 const Portfolio = () => {
   return (
     <Section id="portfolio" title="Check My Portfolio" background="light">
-        <div className="portfolio-content-wrapper"></div>
+        <div className="portfolio-content-wrapper">
+            <Filters />
+        </div>
     </Section>
   )
 }

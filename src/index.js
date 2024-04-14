6ab9270e464a24b6a00d380aba4f3cd2dc70f993
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-04-14 13:31:51 +0530

    :technologist::technologist::technologist:

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-import  ReactDOM  from "react-dom/client";
-import App from "./App";
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App/>);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
